06bae7eb730e93716e8ad7b72e8dc806394957c7
bshapero <bshapero@stanford.edu>
2012-10-23 11:12:00 -0700

    Added some php files.  I have them uploaded also on my afs space.  We may want to set it up so that everything is automatically linked both on afs and locally or just clone the repo there.

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Name</title>
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black">
+		<meta name="viewport" content="width=device-width, user-scalable=no" />
+		<link href="style.css" rel="stylesheet" type="text/css">
+	</head>
+
+	<body>
+		<div class="banner"></div>
+		<?php
+		include("topbar.php");
+		?>
+
+	</body>
+</html>
---
+++ topbar.php
@@ -0,0 +1,11 @@
+<div class="box home">
+	<a href="index.php" class="origin">Home</a>
+</div>
+<div class="box appname">
+	<ins>Name</ins>
+</div>
+<div class="box login">
+	<?php
+		include("login.php");
+	?>
+</div>
