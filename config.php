3bf806623a3992c1c48cd062aacbdcbbfbbd008c
jneighbs <jneighbs@stanford.edu>
2012-10-28 10:28:39 -0700

    working on login - Jacob

--- config.php
+++ config.php
@@ -1,4 +1,16 @@
 <?php
-$link = mysql_connect('mysql-user-master.stanford.edu', 'ccs147bshapero', 'joogoyae');
-mysql_select_db('c_cs147_bshapero');
+$host = "mysql-user-master.stanford.edu";
+$user = "ccs147bshapero";
+$pass = "joogoyae";
+$mysql_database = "c_cs147_bshapero";
+$link = mysql_connect($host, $user, $pass);
+if (!$link) {
+    die('Not connected : ' . mysql_error());
+}
+
+// Connect to the database
+$db_selected = mysql_select_db($mysql_database, $link);
+if (!$db_selected) {
+    die ('Can\'t use to connect to database : ' . mysql_error());
+}
 ?>
