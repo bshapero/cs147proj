a2450a1c9129391ed44fdbc1a6630473162150a9
bshapero <bshapero@stanford.edu>
2012-11-01 18:28:15 -0700

    Fixed mismatched div tags in Profile.

--- profile.php
+++ profile.php
@@ -56,15 +56,13 @@
 	<div data-role="footer" data-id="samebar" class="nav-glyphish-example" data-position="fixed" data-tap-toggle="false">
 		<div data-role="navbar" class="nav-glyphish-example" data-grid="c">
 			<ul>
-				<li><a href="index.php" id="home" data-icon="custom">Home</a></li>
+				<li><a href="index.php" id="home" data-icon="custom" class="ui-btn-active">Home</a></li>
 				<li></li>
 				<li></li>
 				<li><a href="login.php" id="login" data-icon="custom">Login</a></li>
 			</ul>
 		</div>
-	</div>
-</div>
-	
+	</div>	
 
 </script>
 
