d4ee870cab759cf9a1562d6acf7d3bc90d80dbe3
Ryan Ramirez <ryan2013@stanford.edu>
2012-11-04 15:03:05 -0800

    new home page

--- index.php
+++ index.php
@@ -17,41 +17,40 @@
 			$user_email = $_SESSION['id'];
 			echo "<h1> Welcome $user_email :) </h1>";
 		} else { 
-			echo "<h1>NELP!</h1>";
+			echo "<h1>Chirp</h1>";
 		}
 		?>
 		<!-- Ryan: If someone is logged in write "Welcome [username]" -->
 	</div><!-- /header -->
 
 	<div data-role="content">	
-	
-		<?php
-			include("search_form.php");
-		?>
-		<!-- Ryan: Add css so that these lists show up as buttons with logos using good css -->
-		
-		<table class="home-table">
-			<tr>
-				<th><a href="profile.php" id="myprofile" data-icon="custom">My Profile</a></th>
-				<th class="table-cell-middle"><a href="search.php" id="search" data-icon="custom">Search</a></th>
-				<th><a href="review.php" id="writereview" data-icon="custom">Write Review</a></th>
-			</tr>
-		
-			<tr>
-				<td><a href="add_website.php" id="addwebsite" data-icon="custom">Add Website</a></td>
-				<td class="table-cell-middle"><a href="bookmarks.php" id="bookmarks" data-icon="custom">Bookmarks</a></td>
-				<td><a href="about.php" id="about" data-icon="custom">About Nelp</a></td>
-			</tr>
-		</table>
+		<div class="center">
+			<h1>SEARCH</h1>
+			<p>by website</p>
+			<?php
+				include("search_form.php");
+			?>
+			<h1>OR</h1>
+			<p>by category</p>
+			<ul data-role="listview" data-inset="true" data-filter="true">
+				<li><a href="#">News</a></li>
+				<li><a href="#">Social Media</a></li>
+				<li><a href="#">Health Care</a></li>
+				<li><a href="#">Sports</a></li>
+				<li><a href="#">Entertainment</a></li>
+				<li><a href="#">Games</a></li>
+				<li><a href="#">Electronics</a></li>
+			</ul>
+		</div>
 	</div><!-- /content -->
 	
 	<div data-role="footer" data-id="samebar" class="nav-glyphish-example" data-position="fixed" data-tap-toggle="false">
 		<div data-role="navbar" class="nav-glyphish-example" data-grid="c">
 			<ul>
-				<li><a href="index.php" id="home" data-icon="custom" class="ui-btn-active">Home</a></li>
+				<li><a href="index.php" id="search" data-icon="custom" class="ui-btn-active">Search</a></li>
 				<li></li>
-				<li></li>
-				<li><a href="login.php" id="login" data-icon="custom">Login</a></li>
+				<li><a href="profile.php" id="profile" data-icon="custom">My Profile</a></li>
+				<li><a href="bookmarks.php" id="bookmarks" data-icon="custom">Bookmarks</a></li>
 			</ul>
 		</div>
 	</div>
--- search_form.php
+++ search_form.php
@@ -1,4 +1,4 @@
 <form action="search.php" method="post">
-	<label for="foo">Search:</label>
+	<label for="foo"></label>
 	<input type="text" name="search" id="foo">
 </form>
