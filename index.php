ee431e1de3aea871a9b09a952882a209c94eb42c
Ryan Ramirez <ryan2013@stanford.edu>
2012-11-07 15:04:27 -0800

    redo of bottom bar on index page during lab 7

--- index.php
+++ index.php
@@ -85,9 +85,9 @@
 		<div data-role="navbar" class="nav-glyphish-example" data-grid="c">
 			<ul>
 				<li><a href="index.php" id="search" data-icon="custom" class="ui-btn-active">Search</a></li>
-				<li></li>
 				<li><a href="profile.php" id="profile" data-icon="custom">My Profile</a></li>
 				<li><a href="bookmarks.php" id="bookmarks" data-icon="custom">Bookmarks</a></li>
+				<li><a href="login.php" id="login" data-icon="custom">Login</a></li>
 			</ul>
 		</div>
 	</div>
