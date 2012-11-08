2480ec590c9a0798847a6771fe8c99811e1c493b
jneighbs <jneighbs@stanford.edu>
2012-11-07 23:17:46 -0800

    hopefully fixed an annoying bug

--- login_attempt.php
+++ login_attempt.php
@@ -1,6 +1,19 @@
 <?php
 session_start();
-include("config.php");
+$host = "mysql-user-master.stanford.edu";
+$user = "ccs147bshapero";
+$pass = "joogoyae";
+$mysql_database = "c_cs147_bshapero";
+$link = mysql_connect($host, $user, $pass);
+if (!$link) {
+    die('Not connected : ' . mysql_error());
+}
+
+// Connect to the database
+$db_selected = mysql_select_db($mysql_database, $link);
+if (!$db_selected) {
+    die ('Can\'t use to connect to database : ' . mysql_error());
+}
 
 $email = mysql_real_escape_string($_POST['email']);
 $password = md5(mysql_real_escape_string($_POST['password']));
