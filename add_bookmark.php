fccd137eda142c0a9246fb97040c1db240d78b75
bshapero <bshapero@stanford.edu>
2012-11-08 19:41:28 -0800

    now definitely adds to database, but with error.  Still working on that

---
+++ add_bookmark.php
@@ -0,0 +1,16 @@
+
+<?php 
+//	session_start();
+	include("config.php");
+	$site_url = mysql_real_escape_string($_GET["site_url"]);
+	$email = mysql_real_escape_string($_GET["email"]);
+	$query = "SELECT user_id, site_id from Users, Sites WHERE Sites.site_url = '$site_url' AND Users.email = '$email'";
+	$result = mysql_query($query);
+	$rowCheck = mysql_num_rows($result);
+	while ($row = mysql_fetch_array($result)) {
+		$insert = "INSERT INTO Bookmarks values (".$row["user_id"].", ".$row["site_id"].")";
+		$result = mysql_query($insert);
+		echo "Success";
+	}
+
+?>
