74135cfb5073a1a905b45bbbc31ec4d1e8bbc968
bshapero <bshapero@stanford.edu>
2012-11-26 19:09:52 -0800

    Moving custom scripts into own folder.  Can now see selected profile pic.  Not yet uploading

--- edit_profile.php
+++ edit_profile.php
@@ -6,6 +6,7 @@
 	$result = mysql_query($query);
 	$row = mysql_fetch_assoc($result);
 ?>
+    <script src="upload_script.js"></script>
 	<h2>Edit Profile</h2>
 	<?php
 		if (isset($row["user_picture"])) {
@@ -15,8 +16,9 @@
 		}
 	?>
 	<form action="update_profile.php" method="post" class="edit-profile">
-	<input type="file" id="change-pic" name="pic" onchange="upload()" /><br><br>
+	<input type="file" id="image_file" name="image_file" onchange="fileSelected();" /><br><br>
 	Old Password: <input type="password" name="old_password" class="ui-input-text ui-body-c ui-corner-all ui-shadow-inset" />
 	New Password: <input type="password" name="new_password" class="ui-input-text ui-body-c ui-corner-all ui-shadow-inset" /><br>
 	<div data-corners="true" data-shadow="true" data-iconshadow="true" data-wrapperels="span" data-icon="null" data-iconpos="null" data-theme="c" class="ui-btn ui-shadow ui-btn-corner-all ui-submit ui-btn-up-c" aria-disabled="false"><span class="ui-btn-inner ui-btn-corner-all"><span class="ui-btn-text">Save Changes</span></span><input type="submit" value="Save Password Changes" class="ui-btn-hidden" aria-disabled="false"></div><br>
 	</form>
+	<img id="preview" />
