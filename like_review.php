f4b5d884e2a5d959b6251c8fb94385f18be444d0
bshapero <bshapero@stanford.edu>
2012-11-27 21:58:51 -0800

    correctly updates number of likes in the database, but not dynamically changing the page

--- like_review.php
+++ like_review.php
@@ -22,13 +22,19 @@ $rnum  = mysql_num_rows($r);
 if ($rnum == 0) {
 	mysql_query("insert into Likes values('$user_id', '$review_id')");	
 	echo "insert success\n";
-	/*$q2 = "UPDATE Reviews Set Reviews.num_likes = Reviews.num_likes + 1 where review_id = '$review_id'";
+	$q2 = "UPDATE Reviews Set Reviews.num_likes = Reviews.num_likes + 1 where review_id = '$review_id'";
 	$r2 = mysql_query($q2);
 	if (!$r2) {
 		echo mysql_error();	
-	}*/
+	}
 	echo "update success\n";
 } else {
 	mysql_query("delete from Likes where user_id = '$user_id' AND review_id = '$review_id'");
+	$q3 = "UPDATE Reviews Set Reviews.num_likes = Reviews.num_likes - 1 where review_id = '$review_id'";
+	$r3 = mysql_query($q3);
+	if (!$r3) {
+		echo mysql_error();	
+	}
+
 }
 ?> 
